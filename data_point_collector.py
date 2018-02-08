e2f5b051b5e43e1ed343c6b329055fafd5daae44
Ye <yexia@berkeley.edu>
2018-02-08 07:54:45 -0800

    modify data point collector

--- data_point_collector.py
+++ data_point_collector.py
@@ -22,7 +22,10 @@ def read_datasets(data_dir, in_sequences=False):
     
     pickle_filepath = os.path.join(data_dir, pickle_filename)
     if not os.path.exists(pickle_filepath):
-        data_point_names = get_data_point_names(data_dir, in_sequences)
+        directories = ['training', 'validation', 'application']
+        data_point_names = {}
+        for directory in directories:
+            data_point_names[directory] = get_data_point_names(data_dir+directory+'/', in_sequences)
         print ("Pickling ...")
         with open(pickle_filepath, 'wb') as f:
             pickle.dump(data_point_names, f, pickle.HIGHEST_PROTOCOL)
@@ -31,38 +34,33 @@ def read_datasets(data_dir, in_sequences=False):
         with open(pickle_filepath, 'rb') as f:
             data_point_names = pickle.load(f)
 
-    return data_point_names['training'], data_point_names['validation']
+    return data_point_names['training'], data_point_names['validation'], data_point_names['application']
 
-
-def get_data_point_names(data_dir, in_sequences=False, predictionRate=3):
-    if not os.path.isdir(data_dir):
-        print("Data directory '" + data_dir + "' not found.")
+    
+def get_data_point_names(directory, in_sequences=False, predictionRate=3):
+    if not os.path.isdir(directory):
+        print("Data directory '" + directory + "' not found.")
         return None
     
-    data_point_names = {}
-    directories = ['training', 'validation']
-    for directory in directories:
+    image_path = os.path.join(directory, 'camera_images')
+    data_points = set([f[:-4] for f in os.listdir(image_path) if f.endswith('.jpg')])
+        
+    if in_sequences:
         data_point_dict = {}
-        image_path = os.path.join(data_dir, directory, 'camera_images')
-        data_points = set([f[:-4] for f in os.listdir(image_path) if f.endswith('.jpg')])
-        
-        if in_sequences:
-            data_point_dict = {}
-            for data_point in data_points:
-                video_id = data_point.split('_')[0]
-                group = data_point_dict.setdefault(video_id, [])
-                group.append(data_point)
-            data_point_names[directory] = \
-                list(data_point_dict.values())
-        else:
-            data_point_names[directory] = list(data_points)
+        for data_point in data_points:
+            video_id = data_point.split('_')[0]
+            timestamp = int(data_point.split('_')[1])
+            group_key = video_id + str(round(timestamp % (1000/predictionRate)))
             
-        no_of_videos = len(data_point_names[directory])
-        print ('No. of %s videos: %d' % (directory, no_of_videos))
+            group = data_point_dict.setdefault(group_key, [])
+            group.append(data_point)
+        data_point_names = \
+            list(data_point_dict.values())
+    else:
+        data_point_names = list(data_points)
         
-    return data_point_names  
+    no_of_videos = len(data_point_names)
+    print ('No. of %s videos: %d' % (directory, no_of_videos))
     
-    
-    
-    
+    return data_point_names
     
