d4936bf510884ce31fd9369ce33720f4cbf711e6
Ye <yexia@berkeley.edu>
2018-04-16 19:19:11 -0700

    write feature maps from h5 files created by Jinkyu

--- write_tfrecords.py
+++ write_tfrecords.py
@@ -6,6 +6,7 @@ import cv2
 from tqdm import tqdm
 import argparse
 import random
+import h5py
 
 
 import data_point_collector as dpc
@@ -49,12 +50,27 @@ for i in range(len(splits)):
             camera_features = list()
             feature_map_features = list()
             gazemap_features = list()
-            for f in seq:
+            
+            # read feature_maps of one video
+            video_id = seq[0].split('_')[0]
+            feature_maps = h5py.File(os.path.join(feature_folder, video_id+'.h5'), 'r')['X']
+            feature_maps = np.transpose(feature_maps, axes=(0,2,3,1))
+            
+            for j in range(len(seq)):
+                # skip the first 4 frames because each feature map is calculated using 4 frame images
+                if j < 4:
+                    continue
+                # when there are no more feature maps for the frame images, break to next video
+                if j == len(feature_maps):
+                    break
+                
+                f = seq[j]
+                
                 camera = cv2.imread(os.path.join(camera_folder,f+'.jpg'))[:,:,[2,1,0]]
                 camera = cv2.resize(camera, (64,36), interpolation=cv2.INTER_LINEAR)
                 camera_features.append(_bytes_feature(camera.tostring()))
                 
-                feature_map = np.load(os.path.join(feature_folder, f+'.npy'))
+                feature_map = feature_maps[j]
                 feature_map_features.append(_bytes_feature(feature_map.tostring()))
     
                 gazemap = cv2.imread(os.path.join(gazemap_folder, f+'.jpg'))[:,:,0]
